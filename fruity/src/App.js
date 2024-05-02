17864024f3d55ba29c27d0cb317cd855c6e2ae40
Vasudevshetty <vasudeepu2815@gmail.com>
2024-05-03 00:23:25 +0530

    Complete with the markup, Should add styles, and then those logic to it.

--- fruity/src/App.js
+++ fruity/src/App.js
@@ -1,5 +1,81 @@
-function App() {
-  return <p>hello</p>;
+import cane from "./Images/cane.svg";
+import labels from "./Images/Labels.jpg";
+import { fruits } from "./ImagesList";
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <h2 className="logo">Fruity</h2>
+      </header>
+      <main>
+        <div>
+          <Button type="left" />
+          <Button type="right" />
+        </div>
+
+        <Text text="Pear" />
+
+        <div className="section-container-main">
+          <Fruits fruits={["pear", "apple", "exotic"]} />
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default App;
+function Button({ type }) {
+  return (
+    <button id={`${type === "right" ? "next" : "prev"}Button`} className="wave">
+      <i className={`fa-solid fa-chevron-${type}`}></i>
+    </button>
+  );
+}
+
+function Text({ text }) {
+  return (
+    <div className="text">
+      <h1 className="h1">{text}</h1>
+      <div className="cane-image">
+        <img src={cane} alt="Cane" />
+        <img src={labels} alt="Cane" className="cane-labels" />
+      </div>
+    </div>
+  );
+}
+
+function Fruits({ fruits }) {
+  return (
+    <div className="section-container">
+      {fruits.map((fruit, index) => (
+        <Fruit fruit={fruit} secNo={index + 1} key={fruit + (index + 1) + ""} />
+      ))}
+    </div>
+  );
+}
+
+function Fruit({ secNo, fruit }) {
+  return (
+    <section className="section" id={`section${secNo}`}>
+      <Images fruit={fruit} />
+    </section>
+  );
+}
+
+function Images({ fruit }) {
+  return (
+    <div className="fruit-images">
+      {Array.from(["one", "two", "three", "four"]).map((number) => (
+        <Image number={number} fruit={fruit} key={fruit + number} />
+      ))}
+    </div>
+  );
+}
+
+function Image({ number, fruit }) {
+  return (
+    <div className={`image-${number} fruit-image`}>
+      <img src={fruits[fruit][number]} alt={fruit + "-image"} />
+    </div>
+  );
+}
---
+++ fruity/src/ImagesList.js
@@ -0,0 +1,33 @@
+import apple_one from "./Images/apple-one.png";
+import apple_two from "./Images/apple-two.png";
+import apple_three from "./Images/apple-three.png";
+import apple_four from "./Images/apple-three.png";
+import pear_one from "./Images/apple-one.png";
+import pear_two from "./Images/apple-two.png";
+import pear_three from "./Images/apple-three.png";
+import pear_four from "./Images/apple-three.png";
+import exotic_one from "./Images/exotic-one.png";
+import exotic_two from "./Images/exotic-two.png";
+import exotic_three from "./Images/exotic-three.png";
+import exotic_four from "./Images/exotic-three.png";
+
+export const fruits = {
+  apple: {
+    one: apple_one,
+    two: apple_two,
+    three: apple_three,
+    four: apple_four,
+  },
+  pear: {
+    one: pear_one,
+    two: pear_two,
+    three: pear_three,
+    four: pear_four,
+  },
+  exotic: {
+    one: exotic_one,
+    two: exotic_two,
+    three: exotic_three,
+    four: exotic_four,
+  },
+};
