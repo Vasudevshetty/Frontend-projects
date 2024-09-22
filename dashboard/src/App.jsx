c21cf4d042e8bdfdcd77340b82f2a003d598932a
Vasudevshetty <vasudeepu2815@gmail.com>
2024-09-23 00:48:04 +0530

    Completed header part, and also the dark mode thing, got an idea.

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
 function App() {
-  return <div className="text-2xl">apphello</div>;
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <main className={`${darkMode && "dark"}`}>
+      <Header
+        darkMode={darkMode}
+        toggleDarkMode={() => setDarkMode((dark) => !dark)}
+      />
+      <Sidebar />
+    </main>
+  );
 }
 
 export default App;
